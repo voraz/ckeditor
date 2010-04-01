20467f44fed74ac09ef2bf87b0a11eaee54ef07c
Alexandre Bini <alexandre@voraz.com.br>
2010-04-01 10:07:40 -0300

    added image urls funcions

--- lib/ckeditor.rb
+++ lib/ckeditor.rb
@@ -1,4 +1,17 @@
+module Ckeditor
+
+  def ckeditor_images_urls(str)
+    reg = /\< *[img][^\>]*[src] *= *[\"\']{0,1}([^\"\'\ >]*)/i
+    str.scan(reg).uniq.map{|r| r[0]}
+  end
+  def ckeditor_images_names(str)
+    ckeditor_images_urls(str).map{ |r| r.split("/").last }
+  end
+
+end
 if defined?(Rails)
   ActionController::Base.send(:include, CkeditorHelper)
   ActionView::Base.send(:include, CkeditorHelper)
 end
+include Ckeditor
+
